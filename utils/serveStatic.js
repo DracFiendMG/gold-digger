96efd48fb06ab9f5388cd2e500b7399d4f23d12a
DracFiendMG <sreeramrdy@gmail.com>
2026-01-30 23:42:24 +0530

    Implement serveStatic function for serving static files and add routingHandler for GET requests

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,4 +1,28 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { getContentType } from './getContentType'
+import { sendResponse } from './sendResponse'
 
-export function serveStatic(req, res, baseDir) {
-    
+export async function serveStatic(req, res, baseDir) {
+    const publicDir = path.join(baseDir, 'public')
+    const filePath = path.join(
+        publicDir, 
+        req.url === '/'
+        ? 'index.html'
+        : req.url
+    )
+
+    const ext = path.extname(filePath)
+    const contentType = getContentType(ext)
+
+    try {
+
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            const content = await fs.readFile(path.join(publicDir, '404.html'))
+            sendResponse(res, 404, contentType, content)
+        } else {
+            sendResponse(res, 500, 'text/html', '<html><h1>Server Error: ${err.code}</h1></html>')
+        }
+    }
 }
