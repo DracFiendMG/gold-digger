efc726d50f8a0e4d9d400e62e8419c1df73ee55d
DracFiendMG <sreeramrdy@gmail.com>
2026-01-31 19:56:56 +0530

    Refactor serveStatic function to improve file path handling and update error responses

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import http from 'node:http';
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import { getContentType } from './utils/getContentType.js';
+import { serveStatic } from './utils/serveStatic.js';
 
 const PORT = 8000
 
@@ -9,22 +10,15 @@ const __dirname = import.meta.dirname
 const publicDir = path.join(__dirname, 'public')
 
 const server = http.createServer(async (req, res) => {
-    const filePath = path.join(publicDir,
-        req.url === '/'
-        ? 'index.html'
-        : req.url
-    )
 
     if (req.url === '/favicon.ico') {
         res.statusCode = 204
         return res.end()
     }
 
-    const data = await fs.readFile(filePath)
-    const ext = path.extname(filePath)
-    res.statusCode = 200
-    res.setHeader('Content-Type', getContentType(ext))
-    res.end(data)
+    if (!req.url.startsWith('/api')) {
+        serveStatic(req, res, publicDir)
+    }
 })
 
 server.listen(PORT, () => {
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,12 +1,11 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
-import { getContentType } from './getContentType'
-import { sendResponse } from './sendResponse'
+import { getContentType } from './getContentType.js'
+import { sendResponse } from './sendResponse.js'
 
 export async function serveStatic(req, res, baseDir) {
-    const publicDir = path.join(baseDir, 'public')
     const filePath = path.join(
-        publicDir, 
+        baseDir, 
         req.url === '/'
         ? 'index.html'
         : req.url
@@ -16,13 +15,14 @@ export async function serveStatic(req, res, baseDir) {
     const contentType = getContentType(ext)
 
     try {
-
+        const content = await fs.readFile(filePath)
+        sendResponse(res, 200, contentType, content)
     } catch (err) {
         if (err.code === 'ENOENT') {
-            const content = await fs.readFile(path.join(publicDir, '404.html'))
+            const content = await fs.readFile(path.join(baseDir, '404.html'))
             sendResponse(res, 404, contentType, content)
         } else {
-            sendResponse(res, 500, 'text/html', '<html><h1>Server Error: ${err.code}</h1></html>')
+            sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err.code}</h1></html>`)
         }
     }
 }
